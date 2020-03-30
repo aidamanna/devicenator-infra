3796fdb74067991ab19de959fbd3269e5957a31f
Aida Manna <aidamanna@gmail.com>
2020-03-30 20:55:34 +0200

    Move dns record creation under devicenator-ui

--- terraform/1-infrastructure/2-dns/main.tf
+++ terraform/1-infrastructure/2-dns/main.tf
@@ -3,22 +3,6 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_s3_bucket" "devicenator_ui" {
-  bucket = "devicenator.com"
-}
-
 resource "aws_route53_zone" "devicenator" {
   name = "devicenator.com"
-}
-
-resource "aws_route53_record" "devicenator" {
-  zone_id = aws_route53_zone.devicenator.zone_id
-  name    = "devicenator.com"
-  type    = "A"
-
-  alias {
-    name    = data.aws_s3_bucket.devicenator_ui.website_domain
-    zone_id = data.aws_s3_bucket.devicenator_ui.hosted_zone_id
-    evaluate_target_health = false
-  }
 }
--- terraform/2-devicenator-ui/main.tf
+++ terraform/2-devicenator-ui/main.tf
@@ -14,6 +14,22 @@ resource "aws_s3_bucket" "devicenator_ui" {
   }
 }
 
+data "aws_route53_zone" "devicenator" {
+  name = "devicenator.com"
+}
+
+resource "aws_route53_record" "devicenator" {
+  zone_id = data.aws_route53_zone.devicenator.zone_id
+  name    = "devicenator.com"
+  type    = "A"
+
+  alias {
+    name    = aws_s3_bucket.devicenator_ui.website_domain
+    zone_id = aws_s3_bucket.devicenator_ui.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
 resource "aws_iam_user" "travis" {
   name = "travis"
 }
