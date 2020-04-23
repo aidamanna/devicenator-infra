9153737ea3f9b60d52f1b65b1c246d228555e25b
Aida Manna <aidamanna@gmail.com>
2020-04-23 18:49:52 +0200

    Improve state bucket creation

--- terraform/1-infrastructure/1-statebucket/apply.sh
+++ terraform/1-infrastructure/1-statebucket/apply.sh
@@ -5,16 +5,19 @@ mv backend.tf backend.tf.backup
 set -euxo pipefail
 rm -rf ./.terraform
 terraform init
-terraform apply
+terraform plan --out="devicenator-infra.plan"
+terraform apply "devicenator-infra.plan"
 mv backend.tf.backup backend.tf
 
 echo "Enabling remote state"
 rm -rf ./.terraform
 terraform init
-terraform apply
+terraform plan --out="devicenator-infra.plan"
+terraform apply "devicenator-infra.plan"
 
 echo "Cleaning up old local states"
-rm ./terraform.tfstate
-rm ./terraform.tfstate.backup
+rm devicenator-infra.plan
+rm terraform.tfstate
+rm terraform.tfstate.backup
 
 echo "Successfully enabled terraform remote state"
--- terraform/1-infrastructure/1-statebucket/main.tf
+++ terraform/1-infrastructure/1-statebucket/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   profile = "default"
-  region = var.region
+  region = local.region
 }
 
 resource "aws_s3_bucket" "terraform_state_bucket" {
-  bucket = "devicenator-terraform-state-bucket"
+  bucket = "devicenator-state-bucket"
   versioning {
     enabled = true
   }
@@ -15,7 +15,7 @@ resource "aws_s3_bucket" "terraform_state_bucket" {
 }
 
 resource "aws_dynamodb_table" "terraform_state_lock" {
-  name = "devicentator-terraform-state-lock"
+  name = "devicentator-state-lock"
   hash_key = "LockID"
   read_capacity = 20
   write_capacity = 20
